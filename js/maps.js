e3c9975d3dd2ad2cefe961c173640f51c0078e4c
David Kelly <treellane@yahoo.co.uk>
2019-03-23 15:42:51 +0000

    coding in the map buttons!

--- js/maps.js
+++ js/maps.js
@@ -77,17 +77,72 @@ var btnDiv = document.getElementById("buttonDiv");
 var btns = btnDiv.getElementsByClassName("btn");
 
 for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
-  });
-}
-     
-     
-     
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+   
+    }
+    );
+}    
 /* ------------------SELECT/DESELECT MAP MAKRERS------------------------------*/
 
+var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.86, lng: 151.209},
+          zoom: 13,
+          mapTypeControl: false
+        });
 
-  
+        // Add controls to the map, allowing users to hide/show features.
+        var buttonControl = document.getElementById('map-control');
+        map.controls.push(buttonControl);
+
+
+        // Apply new JSON when the user chooses to hide/show features.
+        document.getElementById('hide-attract').addEventListener('click', function() {
+          map.setOptions({styles: attractStyle['hide']});
+        });
+        document.getElementById('show-attract').addEventListener('click', function() {
+          map.setOptions({styles: attractStyle['show']});
+        });
+      }
+
+  var attractStyle = {
+       hide: [
+          {
+            featureType: 'poi.attraction',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'off'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'off'}]
+          },
+          {
+            featureType: 'poi.place_of_worship',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'off'}]
+          }
+        ],
+        show: [
+          {
+            featureType: 'poi.attraction',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'on'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'on'}]
+          },
+          {
+            featureType: 'poi.place_of_worship',
+            elementType: 'labels.icon',
+            stylers: [{visibility: 'on'}]
+          }
+        ]
+      };
       
