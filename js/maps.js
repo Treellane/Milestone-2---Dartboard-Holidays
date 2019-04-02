164707e2983bb69218a8868a28f85287f080c6f1
David Kelly <treellane@yahoo.co.uk>
2019-04-02 18:48:07 +0000

    minor changed and tweeked to code to try to get it working!

--- js/maps.js
+++ js/maps.js
@@ -1,198 +1,157 @@
 
 
-/* ----------GOOGLE AUTOCOMPLTE / SEARCH INPUT FOR MAP------------------------*/
+/*---GOOGLE AUTOCOMPLTE / SEARCH INPUTtaken from Google Maps Paltform---------*/
 
-function initMap() {
+function initAutocomplete() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.86, lng: 151.209},
-          //center: {lat: 47.0000, lng: 7.0000},
-          zoom: 14,
-          //zoom: 4,
+          center: {lat: 15.0, lng: 0.0},
+          zoom: 1,
+          mapTypeId: 'roadmap'
         });
 
-   // Create the search box and link it to the UI element.
+        // Create the search box and link it to the UI element.
         var input = document.getElementById('pac-input');
-
-        var box = document.getElementById('input-box');
-      
-        map.controls.push(box);
-
-        var autocomplete = new google.maps.places.Autocomplete(input);
-
-      // Bind the map's bounds (viewport) property to the autocomplete object,
-      // so that the autocomplete requests use the current map bounds for the
-      // bounds option in the request.
-        autocomplete.bindTo('bounds', map);
-
-      // Set the data fields to return when the user selects a place.
-        autocomplete.setFields(
-            ['address_components', 'geometry', 'icon', 'name']);
-
-var infowindow = new google.maps.InfoWindow();
-        var infowindowContent = document.getElementById('infowindow-content');
-        infowindow.setContent(infowindowContent);
-        var marker = new google.maps.Marker({
-          map: map,
-          anchorPoint: new google.maps.Point(0, -29)
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); //this line causes errors!!!... if i remove
+                                                                        // the [bracketed] section to allow me to moce the
+                                                                        // input box to a different location!!!
+     
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
         });
 
-        autocomplete.addListener('place_changed', function() {
-          infowindow.close();
-          marker.setVisible(false);
-          var place = autocomplete.getPlace();
-          if (!place.geometry) {
-    // User entered the name of a Place that was not suggested and
-    // pressed the Enter key, or the Place Details request failed.
-            window.alert("No details available for input: '" + place.name + "'");
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
             return;
           }
 
-    // If the place has a geometry, then present it on a map.
-          if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-          } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(4);  
-          }
-          marker.setPosition(place.geometry.location);
-          marker.setVisible(true);
+          // Clear out the old markers.
+          markers.forEach(function(marker) {
+            marker.setMap(null);
+          });
+          markers = [];
 
-          var address = '';
-          if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-          }
+          // For each place, get the icon, name and location.
+          var bounds = new google.maps.LatLngBounds();
+          places.forEach(function(place) {
+            if (!place.geometry) {
+              console.log("Returned place contains no geometry");
+              return;
+            }
+            var icon = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
 
-          infowindowContent.children['place-icon'].src = place.icon;
-          infowindowContent.children['place-name'].textContent = place.name;
-          infowindowContent.children['place-address'].textContent = address;
-          infowindow.open(map, marker);
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+              map: map,
+              icon: icon,
+              title: place.name,
+              position: place.geometry.location,
+            }));
+
+            if (place.geometry.viewport) {
+              // Only geocodes have viewport.
+              bounds.union(place.geometry.viewport);
+            } else {
+              bounds.extend(place.geometry.location);
+            }
+          });
+          map.fitBounds(bounds);
         });
-
-} //closing bracket for the initMap funcction
-
+        
+}  // closing bracket of initAutocomplete function
+  
      
  
 /* ------------------SELECT/DESELECT MAP MARKERS------------------------------*/
 
       
-function showAccommidation() {
- alert("Hello world!");
- 
- var NewMapCenter = map.getCenter();
+function showAccommidation() {    alert("Test to see is funtion is entered!");
+    
+      
+  // Getting the co-ords for the center of the map to use at a
+  // starting point for searching and placing markers .... BUT CANT GET IT TO WORK!!! 
   
-  var mapLat = NewMapCenter.lat(); 
-  var mapLng = NewMapCenter.lng();
+  //    var mapCenter = map.getCenter();
+  //    var mapLat = mapCenter.lat(); 
+  //    var mapLng = mapCenter.lng();
+    
+ //     var map = new google.maps.Map(document.getElementById('map'), {
+ //         center: {mapLat, mapLng},
+ //         zoom: 4,
+  //      });
   
-  var map = new google.maps.Map(document.getElementById('map'), {
-          center: {mapLat, mapLng},
-          zoom: 3,
-        });
-  
-  var infoWindow;
-  infoWindow = new google.maps.InfoWindow();
-       
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          radius: 2000,
-          type: ['hotel']
-        }, callback);
-  
-  function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+  // searching for HOTELS within a radius of 1000  
+      var infoWindow;
+      infoWindow = new google.maps.InfoWindow();
+           
+      var service = new google.maps.places.PlacesService(map);
+          service.nearbySearch({
+            location: location,
+            radius: 1000,
+            type: ['hotel']
+          }, callback);
+    
+    //plotting the results on the map
+      function callback(results, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+              createMarker(results[i]);
+            }
           }
         }
-      }
-
+  
       function createMarker(place) {
-       // var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infoWindow.setContent(place.name);
-          infoWindow.open(map, this);
-        });
-      }
- 
-}    
+ // var placeLoc = place.geometry.location;
+         var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location
+          });
   
-
-
-
-function hideAttraction() { 
+          google.maps.event.addListener(marker, 'click', function() {
+            infoWindow.setContent(place.name);
+            infoWindow.open(map, this);
+          });
+        }
+  }    
   
-}
-
-function showBar() { 
   
-}
-
-function hideBar() { 
   
-}
-
-function showRestaurant() { 
+  function hideAccommidation() { 
+    
+  }
+    
   
-}
-
-function hideRestaurant() { 
+  function hideAttraction() { 
+    
+  }
   
-}
-
-function hideAccommidation() { 
+  function showBar() { 
+    
+  }
   
-}
+  function hideBar() { 
+    
+  }
+  
+  function showRestaurant() { 
+    
+  }
+  
+  function hideRestaurant() { 
+    
+  }
+  
         
-
-function shwAccommidation() {
   
-  var NewMapCenter = map.getCenter();
-  
-  var mapLat = NewMapCenter.lat(); 
-  var mapLng = NewMapCenter.lng();
-  
-  var map = new google.maps.Map(document.getElementById('map'), {
-          center: {mapLat, mapLng},
-          zoom: 3,
-        });
-  
-  var infoWindow;
-  
-  infoWindow = new google.maps.InfoWindow();
-       
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          radius: 2000,
-          type: ['hotel']
-        }, callback);
-  
-  function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
-
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: placeLoc
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      }
- 
-}    
